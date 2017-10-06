1147a5bb5ef4cd1191fe2cb3fc8f182b570a37b4
Jason Howmans <jason@nosaj.io>
2017-10-06 23:20:35 +0100

    Add width & height scaling

--- Contents/Sketch/index.js
+++ Contents/Sketch/index.js
@@ -12,6 +12,20 @@ function stepY(context) {
   moveSelectionBy(context, 0, moveBy);
 }
 
+function scaleWidth(context) {
+  const sketch = context.api();
+  const steps = showInputPopover(context, 'Width steps...', 1);
+  const scaleTo = calcStep(context, steps);
+  scaleWidthTo(context, scaleTo);
+}
+
+function scaleHeight(context) {
+  const sketch = context.api();
+  const steps = showInputPopover(context, 'Height steps...', 1);
+  const scaleTo = calcStep(context, steps);
+  scaleHeightTo(context, scaleTo);
+}
+
 function setExponent(context) {
   const sketch = context.api();
   const defaultValue = sketch.settingForKey('ModularScaleExponent') || '1.414';
@@ -61,6 +75,42 @@ function calcStep(context, s) {
   const exponent = parseFloat( sketch.settingForKey('ModularScaleExponent') );
   const base = parseFloat( sketch.settingForKey('ModularScaleBase') );
   return (base * scale(s, exponent)).toFixed(2);
+}
+
+/**
+ * Change width of layer to value
+ * @param {} context
+ * @param {number} newWidth
+ */
+function scaleWidthTo(context, newWidth) {
+  const { selection } = context;
+  selection.forEach(layer => {
+    const layerFrame = layer.frame();
+    const widthHeight = [parseFloat(layerFrame.width()), parseFloat(layerFrame.height())];
+    // Figure out the ratio of height:width so that the shape looks good when scaled
+    const widthHeightRatio = widthHeight[1] / widthHeight[0];
+    const newHeight = parseFloat( newWidth * widthHeightRatio );
+    layer.frame().setWidth(newWidth);
+    layer.frame().setHeight(newHeight);
+  })
+}
+
+/**
+ * Change height of layer to value
+ * @param {} context
+ * @param {number} newWidth
+ */
+function scaleHeightTo(context, newHeight) {
+  const { selection } = context;
+  selection.forEach(layer => {
+    const layerFrame = layer.frame();
+    const heightWidth = [parseFloat(layerFrame.height()), parseFloat(layerFrame.width())];
+    // Figure out the ratio of height:width so that the shape looks good when scaled
+    const heightWidthRatio = heightWidth[1] / heightWidth[0];
+    const newWidth = parseFloat( newHeight * heightWidthRatio );
+    layer.frame().setWidth(newWidth);
+    layer.frame().setHeight(newHeight);
+  })
 }
 
 /**
