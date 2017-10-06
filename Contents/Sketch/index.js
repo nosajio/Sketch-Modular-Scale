3e2b7868c72d3fb142ae80c8d400047ecc73ac1f
Jason Howmans <jason@nosaj.io>
2017-10-06 22:46:12 +0100

    Change settings for Base & Exponent

--- Contents/Sketch/index.js
+++ Contents/Sketch/index.js
@@ -1,19 +1,29 @@
-// Some hardcoded defaults
-const exponent = 1.414;
-const base = 18; // px
-
 function stepX(context) {
   const sketch = context.api();
   const steps = showInputPopover(context, 'Y Steps...', 1);
-  const moveBy = calcStep(steps);
+  const moveBy = calcStep(context, steps);
   moveSelectionBy(context, moveBy, 0);
 }
 
 function stepY(context) {
   const sketch = context.api();
   const steps = showInputPopover(context, 'Y Steps...', 1);
-  const moveBy = calcStep(steps);
+  const moveBy = calcStep(context, steps);
   moveSelectionBy(context, 0, moveBy);
+}
+
+function setExponent(context) {
+  const sketch = context.api();
+  const defaultValue = sketch.settingForKey('ModularScaleExponent') || '1.414';
+  const value = showInputPopover(context, 'Exponent number (ratio)', '1.414');
+  sketch.setSettingForKey('ModularScaleExponent', value);
+}
+
+function setBase(context) {
+  const sketch = context.api();
+  const defaultValue = sketch.settingForKey('ModularScaleBase') || '18';
+  const value = showInputPopover(context, 'Base number (pixels)', defaultValue);
+  sketch.setSettingForKey('ModularScaleBase', value);
 }
 
 function showInputPopover(context, message = '', initialValue = '') {
@@ -46,7 +56,10 @@ function scale(step, exp) {
  * Calculate the amount to move based on step
  * @param {number} s
  */
-function calcStep(s) {
+function calcStep(context, s) {
+  const sketch = context.api();
+  const exponent = parseFloat( sketch.settingForKey('ModularScaleExponent') );
+  const base = parseFloat( sketch.settingForKey('ModularScaleBase') );
   return (base * scale(s, exponent)).toFixed(2);
 }
 
